f0df668488f8a6aa194faf7ae147b0e41cdaf797
James Dunn <jwdunn1@gmail.com>
2017-09-07 14:14:04 -0700

    Post-submission updates for final track
    Remove timer class dupe

--- src/main.cpp
+++ src/main.cpp
@@ -120,42 +120,6 @@ bool straightSegment(double car_s, double len,
   return dlcEnabled;
 }
 
-// Timer1 class
-Timer1::Timer1() {
-  start();
-  beginning_ = start_;
-}
-Timer1::~Timer1() {}
-
-void Timer1::start() {
-  start_ = high_resolution_clock::now();
-}
-
-void Timer1::restart() {
-  start();
-  beginning_ = high_resolution_clock::now();
-}
-
-Timer1::ms Timer1::duration() {
-  high_resolution_clock::time_point now = high_resolution_clock::now();
-  Timer1::ms durat = std::chrono::duration_cast<ms>(now - start_);
-  start_ = now;
-  return durat;
-}
-
-Timer1::ms Timer1::since() {
-  high_resolution_clock::time_point now = high_resolution_clock::now();
-  Timer1::ms durat = std::chrono::duration_cast<ms>(now - start_);
-  return durat;
-}
-
-long Timer1::sinceBeginning() {
-  high_resolution_clock::time_point now = high_resolution_clock::now();
-  Timer1::ms t = std::chrono::duration_cast<ms>(now - beginning_);
-  return t.count();
-}
-
-
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
